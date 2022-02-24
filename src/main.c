45d8acf88ea156b0b5149a94171415023f45d7c6
Rob Rohan <rob@therohans.com>
2022-02-25 08:03:55 +1300

    Using rand in the example

--- src/main.c
+++ src/main.c
@@ -2,19 +2,21 @@
 #include "wefx.h"
 
 /*
-  - the top-left is (0,0) such that pixel coordinates increase in the right and 
-  	down directions (e.g. that the pixel at (7,4) is directly above the pixel 
-  	at (7,5))
-  - the pixel centers have integer coordinates.
-*/
+ * - the top-left is (0,0) and the pixel coordinates increase
+ * in the right and down directions
+ * - the pixel centers have integer coordinates.
+ */
 
+// Called once at startup
 EXPORT void init()
 {
     wefx_open(640, 480, "Test Window");
     wefx_clear_color(0xc0, 0xc0, 0xc0);
     wefx_clear();
+    srand(9999991);
 }
 
+// Called every frame
 EXPORT void main_loop(float time)
 {
     int itime = (int)time;
@@ -29,10 +31,12 @@ EXPORT void main_loop(float time)
     wefx_color(0, 0, 0xff);
     wefx_point(itime % 640, 260);
 
-    wefx_color(0xff, 0xff, 0xff);
+	wefx_color(0xff, 0xff, 0xff);
     wefx_line(0, 0, 640, 480);
-    wefx_line(0, 480, 640, 0);
-    // wefx_line(120, 10, 220, 380);
+    wefx_line(0, 480, 640, 0);  
+
+    wefx_color(rand() % 0xff, rand() % 0xff, rand() & 0xff);
+    wefx_line(320, 240, abs(rand() % 640), abs(rand() % 480));
 
     if (itime % 640 == 0)
     {
