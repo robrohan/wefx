83da3b182a1aa9f8cc4400309829b3ae4cf97ffe
Rob Rohan <rob@therohans.com>
2022-04-14 07:54:35 +1200

    Adding Narrative (#3)
    * Start updating the code so it'll work with narrative
    * First pass using my narrative tool to create the manual
    * Using Narrative tool, still needs work, but seems to be working

--- src/wasm.h
+++ src/wasm.h
@@ -1,8 +1,18 @@
+/*
+
+# Export Function to Javascript
+
+Here we simply define a macro that will let the compiler know
+that a function should be made available in Javascript.
+
+Prefix functions with the _EXPORT_ keyword.
+
+*/
 #ifndef WEFX_WASM_H
 #define WEFX_WASM_H
 
-#include "walloc.h"
 #include "math.h"
+#include "walloc.h"
 
 // This is used to flag a function as something that
 // should be accessible in javascript.
